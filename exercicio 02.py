1eb974a425257cb85a406535077994f5ec855852
Kaique-Pinheiro <kaiquepinheiroo099@gmail.com>
2025-02-17 21:19:54 -0300

    exercicio 02

---
+++ exercicio 02.py
@@ -0,0 +1,4 @@
+km = int(input("digite a quantidade de kilometros percorridos: "))
+dia = int(input("digite a quantidade de dia: "))
+custo = (dia * 60) + (km * 0.15)
+print(f"total a pagar: {custo:.2f}")
