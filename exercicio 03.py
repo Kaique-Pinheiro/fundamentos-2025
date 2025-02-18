ae574e222a629b9fa6eac9c39fa29116bf5a01a9
Kaique-Pinheiro <kaiquepinheiroo099@gmail.com>
2025-02-17 21:36:04 -0300

    exercicio 03 realizado com sucesso

---
+++ exercicio 03.py
@@ -0,0 +1,6 @@
+dias = int(input("digite a quantidade de dias"))
+horas = int(input("digite a quantidade de horas"))
+minutos = int(input("digite a quantidade de minutos"))
+segundos = int(input("digite a quantidade de segundos"))
+tempo = dias*24*60*60 + horas*60*60 + minutos*60 + segundos
+print (f"o tempo total foi de: {tempo} segundos") 
