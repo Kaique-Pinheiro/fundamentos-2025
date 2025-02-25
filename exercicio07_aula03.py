4b8e7972c334971f7cbf3fcd760553e983c8efcc
Kaique-Pinheiro <kaiquepinheiroo099@gmail.com>
2025-02-25 00:59:19 +0000

    exercicio07_aula03

---
+++ exercicio07_aula03.py
@@ -0,0 +1,8 @@
+distancia = int(input("digite a distancia em km: "))
+
+if distancia <= 200:
+     custo = distancia * 0.5
+else:
+     custo = distancia * 0.45
+
+print("o custo da viagem é de R$ %.2f" % custo)
